e46426b3baa8387447f552d6286d9ac59510cc03
unknown <ibmjavanh16@iiht.tech>
2021-04-26 17:06:34 +0530

    integrated with rest api

--- src/userform/userform.js
+++ src/userform/userform.js
@@ -1,22 +1,51 @@
 import {useState} from "react";
-
+import axios from "axios";
 function Userform(){//component ,controller
+    //let state={name:'Ram};
     //model
-    const[userform, setUserform]=useState({
-        name:'Ram'
+    const[userform, setUserform]=useState({//useState=hook, predefined function
+        name:'Ram',
+        mobileNumber:9876786543,
+        email:"ram@gmail.com"
     })
-    return(//view
+    return(//view, jsx
            <div>
-             <input value={userform.name} onChange={handleEvent}></input>
+             <input value={userform.name} name='name' onChange ={updateState}></input>
+             <input placeholder="mobileNumber" value={userform.mobileNumber} name='mobilenumber' onChange={updateState} ></input>
+             <input value={userform.email} name='email' onChange ={updateState}></input>
+             <button onClick={save}>Save</button>
               </div>
         
     )
-    function handleEvent(event){
+    function save(event){
         console.log(event);
-        setUserform({
-            name:event.target.value
-        })
+        axios.post('http://localhost:8081/user',userform)//fetch
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+       
+          
+        console.log(userform.name);
+        console.log(userform.mobileNumber);
+        
         
     }
-}
+    function updateState(event){
+        //console.log(event);
+       
+            const current = {...userform,
+            [event.target.name]:event.target.value ,
+        };
+        console.log(current);
+        
+        setUserform( current);
+       
+    }
+    }
+   
+    
+
 export default Userform;
